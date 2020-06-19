db9813ba67168fdef760ebcdcc8983557327a655
DESKTOP-A2TR5DO\Jungho <luckjungho@gmail.com>
2020-06-19 10:52:29 +0900

    init

---
+++ app/src/main/java/com/zs/test/daggertestapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.zs.test.daggertestapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zs.test.daggertestapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
